c72cf8e167182866443670d7d24df7c2f2a2c627
goldfishl <sicnu.long@gmail.com>
2023-02-04 02:45:00 +0800

    add `combined_prescription` generation and rule based evaluation

---
+++ src/rule_based_evaluation.py
@@ -0,0 +1,34 @@
+# Evaluation (Sequential)
+import tqdm
+from inference import Discriminator
+
+if __name__ == "__main__":
+    model_path = './models/audio_model.pth'
+    labels_csv = './src/class_labels_indices.csv'
+    model = Discriminator(model_path, labels_csv)
+
+    total_samples = 2176
+    # total_samples = 100
+
+    file_pref = "data/combined_prescription/evaluation/"
+
+    all_samples = [(file_pref+"sample_" + str(i) + ".wav",
+                    "".join(open(file_pref+"sample_" + str(i) + ".txt"))) for i in range(total_samples)]
+    all_samples = [(a, b.strip().split("\t"))for a, b in all_samples]
+
+    correct = 0
+    total_pred = 0
+    total_ref = 0
+
+    for src, ref in tqdm.tqdm(all_samples):
+        pred = model.inference(src)
+        correct += sum([1 for item in pred if item in ref])
+        total_pred += len(pred)
+        total_ref += len(ref)
+
+    prec = correct / total_pred
+    recl = correct / total_ref
+    f1 = 2 * prec * recl / (prec + recl)
+    print("Precision: %f" % (correct / total_pred))
+    print("Recall:    %f" % (correct / total_ref))
+    print("F1:        %f" % (f1))
